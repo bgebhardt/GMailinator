8a273b35be8819b793dc9700443387b8d94820b5
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2016-09-26 17:42:23 -0700

    Added a check for the UUID in the new info.plist item in macOS Sierra.

--- find_uuid.sh
+++ find_uuid.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
+
+echo "Reading the mail app uuid for Mail Version 9.3 or earlier..."
 defaults read /Applications/Mail.app/Contents/Info PluginCompatibilityUUID
+
+# With macOS Sierra there's a new UUID to look up
+# See [Doesn't work with Mail 10. · Issue #43 · nompute/GMailinator]( https://github.com/nompute/GMailinator/issues/43#issuecomment-245329386 )
+echo "Reading the mail app uuid for Mail Version (macOS Sierra) or later..."
+defaults read /Applications/Mail.app/Contents/Supported10.12PluginCompatibilityUUIDs
